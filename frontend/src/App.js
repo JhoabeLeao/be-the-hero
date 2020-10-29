c110c2f848e8a2ed150e6af271cfe0dbd94ff756
unknown <you@example.com>
2020-10-28 23:06:52 -0300

    Alterações dia 28/10

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,12 @@
-import React  from 'react';
+import React from 'react';
+
 
 import './global.css';
-
 import Logon from './pages/Logon';
 
 function App(){
     return (
-        <Logon />      
+            <Logon />       
     );
 }
 
