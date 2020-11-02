8d0f6becf631a7f55e01793f0537fca086c46e22
unknown <you@example.com>
2020-11-02 19:37:26 -0300

    Alteração depois de resolver problemas com o código.

--- backend/src/controllers/IncidentController.js
+++ backend/src/controllers/IncidentController.js
@@ -27,15 +27,16 @@ module.exports = {
         });
         return response.json({ id });
     },
-    async delete(request, response){
-        const {id} = request.params;
+    async delete(request, response) {
+        const { id } = request.params;
         const ong_id = request.headers.authorization;
+
         const incident = await connection('incidents')
             .where('id', id)
             .select('ong_id')
             .first();
         
-        if (incident.org_id != ong_id){
+        if (incident.ong_id != ong_id){
             return response.status(401).json ({error: 'Operation not permited.'});
         }
 
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,11 @@
 import React from 'react';
-
-
 import './global.css';
-import Logon from './pages/Logon';
 
-function App(){
+import Routes from './routes';
+
+function App() {
     return (
-            <Logon />       
+        <Routes />       
     );
 }
 
